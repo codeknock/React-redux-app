893c2c44b2875f1d0a00166b6f75fa94c8dc93f9
chandrakant nyaupane <chandra@MacBook-Pro.local>
2021-12-30 13:55:22 +0200

    products,Listing and detail setup done

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,18 +1,21 @@
 import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
-    products: [{
+    products: [
+        {
+
         id: 1,
-        title: Chandra,
-        category: developer,
-    }]
+        title: "Chandra",
+        category: "developer",
+    }
+]
 }
 
-export const productReducer = (state, {action: type, payload}) => {
+export const productReducer = (state=initialState, { type, payload }) => {
         switch (type) {
             case ActionTypes.SET_PRODUCTS:
                 return state
             default:
-                break;
+                return state
         }
 }
