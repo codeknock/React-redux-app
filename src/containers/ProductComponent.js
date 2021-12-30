1303a9bb7e5d49008be2f6aeaa4dbde3fabf99b6
chandrakant nyaupane <chandra@MacBook-Pro.local>
2021-12-30 20:42:32 +0200

    products fetched finto home-page

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { useSelector } from "react-redux"
 
+
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products)
-    const { id, title } = products[0]
-    return (
-        <div className="four column wide">
-           <div className="ui link cards">
-               <div className="card">
-                   <div className="image"></div>
-                   <div className="content">
-                       <div className="header">{title}</div>
-                   </div>
-               </div>
-           </div>
+    //const { id, title } = products[0]
+
+    const renderList = products.map((product) => {
+        const { id, title, image, price, category } = product
+        //const classes = useStyles();
+        return (
+           
+            <div className="four wide column" key={id}>
+            <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">$ {price}</div>
+                <div className="meta">{category}</div>
+              </div>
+            </div>
+          </div>
+</div>
             
-        </div>
+        
+        )
+    })
+    return (
+        <>{renderList}</>
     )
 }
 
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+//passing products in action.
+import {setProducts} from "../redux/actions/productActions"
 import ProductComponent from "./ProductComponent"
+
 
 const ProductListing = () => {
     const products = useSelector((state) => state)
-    //console.log(products)
+    //accessing data/products into store
+    const dispatch = useDispatch()
+    
+    const fetchProducts = async () => {
+        const response = await axios
+        .get('https://fakestoreapi.com/products')
+        .catch((err) => {
+            console.log("Err", err)
+        }) 
+        //passing products as setProducts in action through dispatch
+       dispatch(setProducts(response.data)) 
+    };
+
+     useEffect(() => {
+         fetchProducts()
+     }, [])
+     console.log("Products: ", products)
     return (
         <div className="ui grid container">
             <ProductComponent />
